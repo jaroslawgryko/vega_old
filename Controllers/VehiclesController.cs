ea5f82e943c8d58a6d5f92ee3db8fd2874ac9c72
Jaroslaw Gryko <jaroslaw.gryko@gmail.com>
2017-10-23 00:44:48 +0200

    add Vehicles API

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Models;
+using vega.Persistence;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly IMapper mappper;
+        public VegaDbContext context { get; }
+        public VehiclesController(IMapper mapper, VegaDbContext context)
+        {
+            this.context = context;
+            this.mappper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = Mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = Mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
+        }
+    }
+}

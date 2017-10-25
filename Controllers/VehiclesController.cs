1e43766537318b70ef62816415ea59df59960cf6
Jaroslaw Gryko <jaroslaw.gryko@gmail.com>
2017-10-25 22:44:03 +0200

    api to delete vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -52,6 +52,10 @@ namespace vega.Controllers
                 return BadRequest(ModelState);
 
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if(vehicle == null)
+                return NotFound();
+
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -62,5 +66,17 @@ namespace vega.Controllers
             return Ok(result);
         }
         
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id){
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if(vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }
     }
 }
